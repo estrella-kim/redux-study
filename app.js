54167793444b92307be78b752c24774348a083f3
EstrellaJYKIM <puris9030@gmail.com>
2018-01-28 11:10:54 +0900

    dispatch action을 받아 reducer가 실행되고 어떻게 state가 나오는지 test

--- app.js
+++ app.js
@@ -2,4 +2,40 @@
 컨테이너란 ?
 리덕스를 통해 스토어를 만들어야한다.
  */
-// const store = Redux.createStore();
+const store = Redux.createStore(function(state, action) {
+    console.log('reducer:',state);
+    console.log('action:',action);
+    if(action.type === 'PLUS_NUMBER') {
+        return state+1;
+    }
+    else if(action.type === 'MINUS_NUMBER') {
+        return state-1;
+    }
+
+    return state;
+},0);
+//createStore의 첫번재 인자는 reduce
+console.log(store.getState());
+
+store.dispatch({
+    type : 'PLUS_NUMBER',
+
+}) //객체를 넣어야한다. dispatch가 전달해주는 객체를 'action'이라 한다. 이 action은 type property를 가지고 있어야한다. dispatch를 실행할 때마다 reducer가 불린다.
+console.log(store.getState());
+
+store.dispatch({
+    type : 'PLUS_NUMBER',
+
+}) //객체를 넣어야한다. dispatch가 전달해주는 객체를 'action'이라 한다. 이 action은 type property를 가지고 있어야한다. dispatch를 실행할 때마다 reducer가 불린다.
+console.log(store.getState());
+
+store.dispatch({
+    type : 'PLUS_NUMBER',
+
+}) .
+console.log(store.getState());
+store.dispatch({
+    type : 'MINUS_NUMBER',
+
+})
+console.log(store.getState());
